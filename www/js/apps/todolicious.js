e35e56b17adf302aa71c4b46100a8d62af99d5ec
Alexander Schrab <alexander.schrab@gmail.com>
2015-05-31 20:07:10 +0200

    polymer 1.0

--- www/js/apps/todolicious.js
+++ www/js/apps/todolicious.js
@@ -1,12 +1,4 @@
 define(["jquery"],
     function ($) {
-        $("todo-app").get(0).model = {
-             content: "testing",  
-             bullets: [{
-                 content: "sub testing1",
-                 bullets: []
-             }, {
-                 content: "sub testing2",
-                 bullets: []
-             }] };
+        $("todo-app").get(0).model =  };
     });
